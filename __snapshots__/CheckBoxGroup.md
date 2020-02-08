7399b7150f70ee500975ae7fb7d3070818926b99
Brandon Martel <brandonmartel@gmail.com>
2020-02-08 16:36:53 -0600

    fixing tests for checkboxgroup

---
+++ __snapshots__/CheckBoxGroup.md
@@ -0,0 +1,38 @@
+# `CheckBoxGroup`
+
+#### `renders with a label and checkboxes some selected`
+
+```html
+<label class="input-wrapper reversed">
+  <span
+    class="label"
+    id="label-"
+  >
+    Favorite Fruits
+  </span>
+  <div class="select-group">
+    <z-checkbox
+      checked=""
+      label="Apple"
+      reversed=""
+      value="apple"
+    >
+    </z-checkbox>
+    <z-checkbox
+      label="Banana"
+      reversed=""
+      value="banana"
+    >
+    </z-checkbox>
+    <z-checkbox
+      checked=""
+      label="Orange"
+      reversed=""
+      value="orange"
+    >
+    </z-checkbox>
+  </div>
+</label>
+
+```
+
--- test/checkboxgroup.test.js
+++ test/checkboxgroup.test.js
@@ -7,11 +7,6 @@ const options = [
   { label: 'Banana', value: 'banana' },
   { label: 'Orange', value: 'orange' },
 ];
-[
-  { label: 'Apple', value: 'apple' },
-  { label: 'Banana', value: 'banana' },
-  { label: 'Orange', value: 'orange' },
-];
 
 describe('CheckBoxGroup', () => {
   it('renders a label', async () => {
@@ -30,23 +25,85 @@ describe('CheckBoxGroup', () => {
     expect(el.options).to.equal(options);
   });
 
+  it('renders with a value', async () => {
+    const el = await fixture(html`
+      <z-checkboxgroup .options=${options} .value=${['apple', 'orange']}></z-checkboxgroup>
+    `);
+
+    expect(el.value.length).to.equal(2);
+  });
+
+  it('renders with a label and checkboxes some selected', async () => {
+    const el = await fixture(html`
+      <z-checkboxgroup
+        label="Favorite Fruits"
+        .options=${options}
+        .value=${['apple', 'orange']}
+      ></z-checkboxgroup>
+    `);
+
+    expect(el).shadowDom.to.equalSnapshot();
+  });
+
   it('can select an option', async () => {
     const el = await fixture(html`
       <z-checkboxgroup .options=${options}></z-checkboxgroup>
     `);
 
-    el.shadowRoot.querySelector('z-checkbox').click();
+    el.shadowRoot
+      .querySelector('z-checkbox')
+      .shadowRoot.querySelector('input')
+      .click();
 
-    expect(el.value).to.equal(['apple']);
+    expect(el.value[0]).to.equal('apple');
   });
 
-  // it('shows initially the text "hey there Nr. 5!" and an "increment" button', async () => {
-  //   const el = await fixture(html`
-  //     <z-checkboxgroup></z-checkboxgroup>
-  //   `);
+  it('can select multiple options', async () => {
+    const el = await fixture(html`
+      <z-checkboxgroup .options=${options}></z-checkboxgroup>
+    `);
 
-  //   expect(el).shadowDom.to.equalSnapshot();
-  // });
+    const checkboxes = el.shadowRoot.querySelectorAll('z-checkbox');
+
+    checkboxes.forEach((checkbox, index) => {
+      checkbox.shadowRoot.querySelector('input').click();
+      expect(el.value[index]).to.equal(options[index].value);
+    });
+
+    expect(el.value.length).to.equal(options.length);
+  });
+
+  it('can deselect an option', async () => {
+    const el = await fixture(html`
+      <z-checkboxgroup .options=${options} .value=${['apple', 'orange']}></z-checkboxgroup>
+    `);
+
+    const checkbox = el.shadowRoot.querySelector('z-checkbox');
+    checkbox.shadowRoot.querySelector('input').click();
+
+    expect(el.value.length).to.equal(1);
+  });
+
+  it('can deselect multiple options', async () => {
+    const el = await fixture(html`
+      <z-checkboxgroup
+        .options=${options}
+        .value=${['apple', 'banana', 'orange']}
+      ></z-checkboxgroup>
+    `);
+
+    const checkboxes = el.shadowRoot.querySelectorAll('z-checkbox');
+
+    let expectedLength = el.value.length;
+
+    checkboxes.forEach(checkbox => {
+      expectedLength--; // eslint-disable-line no-plusplus
+      checkbox.shadowRoot.querySelector('input').click();
+      expect(el.value.length).to.equal(expectedLength);
+    });
+
+    expect(el.value.length).to.equal(0);
+  });
 
   it('passes the a11y audit', async () => {
     const el = await fixture(html`
