1e37767b3a1bc29fbeed343589cd6e1533230db5
Brandon Martel <brandonmartel@gmail.com>
2020-02-16 18:24:03 -0600

    make import/exports consistent between packages

---
+++ packages/icon/src/icon.js
@@ -0,0 +1,70 @@
+import { svg, css } from 'lit-element';
+import { unsafeSVG } from 'lit-html/directives/unsafe-svg';
+import { AsyncElement } from '@interstice/async';
+
+/**
+ * @element i-icon
+ *
+ * @cssprop --i-icon-color
+ * @cssprop --i-icon-width
+ * @cssprop --i-icon-height
+ */
+export class Icon extends AsyncElement {
+  static get properties() {
+    return {
+      name: String,
+      icons: String,
+      cdn: String,
+      version: String,
+      path: String,
+      size: Number,
+    };
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: inline-flex;
+      }
+      svg {
+        width: var(--i-icon-width, 20px);
+        height: var(--i-icon-height, 20px);
+        fill: var(--i-icon-color, currentColor);
+      }
+    `;
+  }
+
+  constructor() {
+    super();
+    this.name = '';
+    this.icons = 'zondicons';
+    this.cdn = 'https://unpkg.com';
+    this.version = '0.0.7';
+    this.path = '';
+    this.size = 20;
+    this.deps = ['name', 'path'];
+  }
+
+  icon() {
+    return (
+      this.path ||
+      `${this.cdn}/@interstice/icons-${this.icons}@${this.version}/dist-src/${this.name}.js`
+    );
+  }
+
+  async importIcon() {
+    const { default: svgContent } = await import(this.icon());
+    return svgContent;
+  }
+
+  async asyncRender() {
+    const svgContent = await this.importIcon();
+    return svg`
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${this.size} ${this.size}">${unsafeSVG(
+      svgContent,
+    )}</svg>
+    `;
+  }
+}
+
+customElements.define('i-icon', Icon);
